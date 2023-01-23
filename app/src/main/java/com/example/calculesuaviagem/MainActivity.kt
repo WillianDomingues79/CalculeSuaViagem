9d193e2c21dca276ea483d713e34f91e089ecb91
WillianDomingues79 <willian.domingues3@gmail.com>
2023-01-23 06:45:20 -0300

    Criado layout

--- app/src/main/java/com/example/calculesuaviagem/MainActivity.kt
+++ app/src/main/java/com/example/calculesuaviagem/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.calculesuaviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.calculesuaviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.butttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View){
+        if (view.id == R.id.buttton_calculate){
+            calculate()
+        }
+    }
+
+    private fun calculate(){
+        Toast.makeText(this,"Fui Clicado", Toast.LENGTH_SHORT).show()
     }
 }
